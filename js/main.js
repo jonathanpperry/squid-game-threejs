a8444b8f67a541c8e3a5af3051a7c244299065fa
Jonathan Perry <ts-jonathan.perry@rakuten.com>
2021-11-17 11:47:32 +0900

    Finished creating track

--- js/main.js
+++ js/main.js
@@ -15,10 +15,19 @@ renderer.setClearColor(0xb7c3f3, 1);
 const light = new THREE.AmbientLight(0xffffff); // soft white light
 scene.add(light);
 
-// const geometry = new THREE.BoxGeometry();
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
+// Global variables
+const start_position = 3;
+const end_position = -start_position;
+
+function createCube(size, positionX, rotY = 0, color = 0xfbc851) {
+  const geometry = new THREE.BoxGeometry(size.w, size.h, size.d);
+  const material = new THREE.MeshBasicMaterial({ color: color });
+  const cube = new THREE.Mesh(geometry, material);
+  cube.position.x = positionX;
+  cube.rotation.y = rotY;
+  scene.add(cube);
+  return cube;
+}
 
 camera.position.z = 5;
 
@@ -42,6 +51,23 @@ class Doll {
   lookForward() {
     gsap.to(this.doll.rotation, { y: 0, duration: 0.45 });
   }
+}
+
+function createTrack() {
+  createCube(
+    { w: start_position * 2 + 0.2, h: 1.5, d: 1 },
+    0,
+    0,
+    0xe5a716
+  ).position.z = -1;
+  createCube({ w: 0.2, h: 1.5, d: 1 }, start_position, -0.35);
+  createCube({ w: 0.2, h: 1.5, d: 1 }, end_position, 0.35);
+}
+
+createTrack();
+
+class Player {
+  constructor() {}
 }
 
 let doll = new Doll();
